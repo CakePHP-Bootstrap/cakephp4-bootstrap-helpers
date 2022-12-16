ef422a593d22b2cda767bf2a6f86d409bd6cd9da
Erwane Breton <erwane@phea.fr>
2022-12-16 09:25:47 +0100

    fix unknown vars in templates

--- templates/element/flash/error.php
+++ templates/element/flash/error.php
@@ -1,20 +1,20 @@
 <?php
 /**
- *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE file
  * Redistributions of files must retain the above copyright notice.
  * You may obtain a copy of the License at
- *
  *     https://opensource.org/licenses/mit-license.php
- *
  *
  * @copyright Copyright (c) Mikaël Capelle (https://typename.fr)
  * @license https://opensource.org/licenses/mit-license.php MIT License
+ * @var \Cake\View\View $this
+ * @var string $message
+ * @var array $params
  */
 
-$helper = new \Bootstrap\View\Helper\HtmlHelper ($this) ;
+$helper = new \Bootstrap\View\Helper\HtmlHelper($this);
 if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
-echo $helper->alert($message, 'danger', $params) ;
+echo $helper->alert($message, 'danger', $params);
