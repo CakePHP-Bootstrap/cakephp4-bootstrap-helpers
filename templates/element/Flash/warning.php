08a01b46ba6c97d0ef48ea11bdfb3565db68f7d9
Fernando Herrero <ininflamable@gmail.com>
2023-11-23 12:05:03 +0100

    Templates for CakePHP >= 4.4

---
+++ templates/element/Flash/warning.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE file
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mikaël Capelle (https://typename.fr)
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ * @link        https://holt59.github.io/cakephp3-bootstrap-helpers/
+ */
+
+$helper = new \Bootstrap\View\Helper\HtmlHelper ($this) ;
+if (!isset($params['escape']) || $params['escape'] !== false) {
+    $message = h($message);
+}
+echo $helper->alert($message, 'warning', $params) ;
